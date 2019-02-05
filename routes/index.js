bf32f0022588c10d9d097e7470bfdbeb24b96540
Justin Crown <justincrown1@gmail.com>
2019-02-05 10:39:14 -0800

    basic pagination

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,8 @@ var awsregion = process.env.AWS_REGION || 'us-east-1';
 var showBucket = process.env.S3_BUCKET;
 var imgPath = process.env.IMG_PATH;
 var pageTitle = process.env.PAGE_TITLE || 'AWS S3 Image Viewer'; 
-//var AWS = require('aws-sdk');
-//AWS.config.update({accessKeyId: awskey, secretAccessKey: awssecretkey, region: awsregion});
-//var s3 = new AWS.S3();
+const pageSize = process.env.PAGE_SIZE || 2;
+
 const s3 = require('s3-cached')({
   bucket: showBucket,
   s3Options: {
@@ -47,39 +46,41 @@ function buildImagesListFromS3Data(params, images, cb) {
     if (!images) {
         images = [];
     }
-    console.log(params);
+    var page = params.page || 1;
+    if (page == 1) {
+        var startImg = 0;
+        var endImg = pageSize;
+    } else {
+        var startImg = page * pageSize;
+        var endImg = startImg + pageSize;
+    }
     
     s3.getKeysCached(imgPath).then((data) => {
-        console.log('raw data');
-        console.log(data);
         var images = [];
-        //console.log("iterating " + JSON.stringify(contents));
+
         for (var iter in data) {
-            // any validation of key can go here
-            //images.push(S3_PREFIX + contents[iter].Key);
             images.push(data[iter]);
         }
-            console.log('Filtering images by selected date range');
-            console.log(new Date(params['start']));
-            var test_img = images[0];
-            console.log(new Date(test_img.LastModified));
-            //var newimages = images.filter(image => Date(params['start']) >= Date(image.LastModified) && Date(params['end']) <= Date(image.LastModified));
-            if (params['start']) {
-                console.log('Removing images created before ' + params['start']);
-                var images = images.filter(image => new Date(params['start']) <= new Date(image.LastModified));
-                console.log(images);
-            };
-            if (params['end']) {
-                console.log('Removing images created after ' + params['end']);
-                var images = images.filter(image => new Date(params['end']) >= new Date(image.LastModified));
-                console.log(images);
-            };
-            console.log('Sorting images by datetime');
-            images.sort(function(a,b){
-		      return new Date(b.LastModified) - new Date(a.LastModified);
-            });
-            // sort by descending timestamp by default
-            cb(images);
+
+        if (params['start']) {
+            console.log('Removing images created before ' + params['start']);
+            var images = images.filter(image => new Date(params['start']) <= new Date(image.LastModified));
+            console.log(images);
+        };
+        if (params['end']) {
+            console.log('Removing images created after ' + params['end']);
+            var images = images.filter(image => new Date(params['end']) >= new Date(image.LastModified));
+            console.log(images);
+        };
+
+        images.sort(function(a,b){
+          return new Date(b.LastModified) - new Date(a.LastModified);
+        });
+
+        // Paginate
+        var paginatedImages = images.slice(startImg, endImg);
+
+        cb(paginatedImages);
     }).catch((err) => {
         console.log(err, err.stack); // an error occurred
     });
@@ -92,9 +93,11 @@ router.get('/', function(req, res, next) {
 
   var start = req.query.start_date;
   var end = req.query.end_date;
+  var page = req.query.page;
   var params = {
       start: start,
-      end: end
+      end: end,
+      page: page
   }
   var imagesArray = [];
   if (!showBucket) { 
